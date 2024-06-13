8cdce9c5bc35c36cb6bd14d90c8603b6a886a919
achan17 <anthonychietto@gmail.com>
2024-06-12 22:56:48 -0700

    Introduce Chan
    Tested Chan's inputs

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bello!</Text>
+      <Text>Chan was here</Text>
       <StatusBar style="auto" />
     </View>
   );
