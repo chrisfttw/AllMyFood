73cfc60fd8eb31ec944b81d037b75f65daa73a26
chrisfttw <aircjp23@gmail.com>
2024-07-08 20:16:57 -0700

    july8th
    bottom bar icons not working

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router';
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="signin"
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="signup"
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack>
+    </>
   )
 }
 
--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,12 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignUp = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
 
-export default SignUp
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    setIsSubmitting(true);
+    try {
+      const { email, password } = form;
+      await auth().createUserWithEmailAndPassword(email, password);
+      alert('User registered successfully!');
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Text className="text-2xl text-white mt-10">Sign Up</Text>
+
+          <FormField 
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+          />
+
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="emailaddress"
+          />
+
+          <FormField 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton 
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-white">
+              Have an account already?
+            </Text>
+            <Link href="/signin" className="text-lg text-blue-500">Sign In</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
